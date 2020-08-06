ce6ec9215b0948c44a95c1f6047d66a54beb933e
itachi <itachi@itachi.com>
2020-08-06 23:38:33 +0530

    Updates

---
+++ precache-manifest.5f68699a4da7068e0b94be10e6424d96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6a3a1215d9cb79be1ed4f592bf2e72",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "70142f27970883227a20",
+    "url": "/hello-world/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36fb853b09e8c9b74812",
+    "url": "/hello-world/static/js/2.be7e122f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-world/static/js/2.be7e122f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70142f27970883227a20",
+    "url": "/hello-world/static/js/main.a6fa3c48.chunk.js"
+  },
+  {
+    "revision": "ec6ae3aa93fcf95cfb14",
+    "url": "/hello-world/static/js/runtime-main.693b020b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world/static/media/logo.5d5d9eef.svg"
+  }
+]);
